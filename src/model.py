84a6512e56fde2c95111d7d7b09bb64ba3657b2b
manhmitcf <manhmit1221@gmail.com>
2025-03-24 02:03:47 +0700

    update

--- src/model.py
+++ src/model.py
@@ -4,7 +4,7 @@ import torchvision.models as models
 from torchvision.models import ResNet18_Weights
 
 class FishClassifier(nn.Module):
-    def __init__(self):
+    def __init__(self, num_classes=11):  # Thêm tham số num_classes
         super(FishClassifier, self).__init__()
         # Sử dụng weights từ ImageNet
         self.resnet = models.resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
@@ -16,7 +16,7 @@ class FishClassifier(nn.Module):
         # Thay thế Fully Connected Layer với Dropout
         self.resnet.fc = nn.Sequential(
             nn.Dropout(0.5),
-            nn.Linear(self.resnet.fc.in_features, 11)
+            nn.Linear(self.resnet.fc.in_features, num_classes)  # Sử dụng num_classes thay vì cố định là 11
         )
         
     def forward(self, x):
